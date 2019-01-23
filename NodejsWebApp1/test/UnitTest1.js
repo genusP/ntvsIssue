1e6e0ff24a053c978734a93fdb524264d241dd9e
Gennady Pundikov <genusP@outlook.com>
2019-01-23 10:07:50 +0300

    Init

---
+++ NodejsWebApp1/test/UnitTest1.js
@@ -0,0 +1,82 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const assert = require("assert");
+//import { ApiContext } from "../src/index";
+//import { Context, metadata } from "./models";
+//import { fetchUrlInterceptor } from "./infrastucture";
+//import { ApiContextImpl } from '../src/apiContext';
+//import { describe } from 'mocha';
+//var requestInfo: { url?: string, method?: string, payload?: any } = {};
+//const context = new ApiContextImpl(
+//    "/api",
+//    metadata,
+//    {
+//        fetch: fetchUrlInterceptor((url, m, pl) => { requestInfo.url = url; requestInfo.method = m; requestInfo.payload = pl; })
+//    }) as any as ApiContext<Context>;
+//function isGetMethod() {
+//    assert.ok(requestInfo.method === "get", "Request method must be 'get'");
+//}
+//function isPostMethod() {
+//    assert.ok(requestInfo.method === "post", "Request method must be 'post'");
+//}
+describe("EntitySet", () => {
+    it("All", () => {
+        assert.ok(true);
+    });
+    it("By key", () => {
+        assert.ok(true);
+    });
+    it("By string key", () => {
+        assert.ok(true);
+    });
+    it("By named key", () => {
+        assert.ok(true);
+    });
+    it("Navigation", () => {
+        assert.ok(true);
+    });
+    it("Navigation by key", () => {
+        assert.ok(true);
+    });
+    it("Count", () => {
+        assert.ok(true);
+    });
+});
+//describe("Singleton", () => {
+//    it("all", () => {
+//        assert.ok(true);
+//    });
+//    it("Navigation", () => {
+//        assert.ok(true);
+//    });
+//    it("Navigation by key", () => {
+//        assert.ok(true);
+//    });
+//});
+describe("Operations", () => {
+    it("unbound function", () => {
+        assert.ok(true);
+    });
+    it("unbound function with args", () => {
+        assert.ok(true);
+    });
+    it("unbound action", () => {
+        assert.ok(true);
+    });
+    it("unbound action with args", () => {
+        assert.ok(true);
+    });
+    it("bound function", () => {
+        assert.ok(true);
+    });
+    it("bound action", () => {
+        assert.ok(true);
+    });
+    it("collection bound function", () => {
+        assert.ok(true);
+    });
+    it("collection bound action", () => {
+        assert.ok(true);
+    });
+});
+//# sourceMappingURL=UnitTest1.js.map
